c409dc93ba8328ffb7f6ea164350feea952230ce
INDRA\edlopezh <edlopezh@indracompany.com>
2023-03-22 08:06:33 -0500

    coneccion ok

--- database/config.js
+++ database/config.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async () => {
-
-        await mongoose
-            .connect(process.env.MONGODB_CNN)
-            .then(() => console.log("DB funcionando"))
-            .catch((error) => console.error(error));
-    /*try {
+    try {
         await mongoose.connect(process.env.MONGODB_CNN);
         console.log('Base de datos online');
     } catch (error) {
         console.log(error);
         throw new Error('error al conectar la base de datos');
-    }*/
+    }
 }
 
 module.exports = {
