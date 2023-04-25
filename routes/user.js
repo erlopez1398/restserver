088b01cc0b3ef125875289778d1a2e0d46c703b4
INDRA\edlopezh <edlopezh@indracompany.com>
2023-04-25 08:08:47 -0500

    minimos

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+
 const { userGet, userPut, userPost, userDelete, usersPatch } = require('../controllers/user');
 const validarCampos = require('../middlewares/validar-campos');
+const { esRoleValido } = require('../helpers/db-validators');
 
 const router = Router();
 
@@ -12,13 +14,14 @@ router.get('/', userGet);
 
 router.put('/:id', userPut);
 
-router.post('/',[
+router.post('/', [
     check('nombre', 'El nombre es obligatorio').notEmpty(),//tambien puede ser .not().isEmpty()
-    check('password','La contraseña debe ser más de 6 letras').isLength({ min: 6 }),
-    check('correo', 'El correo noe s válido').isEmail(),
-    check('rol','No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('password', 'La contraseña debe ser más de 6 caracteres').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    //check('rol','No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom(esRoleValido),
     validarCampos
-],userPost);
+], userPost);
 
 router.delete('/', userDelete);
 
