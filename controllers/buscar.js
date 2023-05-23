023cd292c1af8807ebfd55a1d308f67c6eca8f42
INDRA\edlopezh <edlopezh@indracompany.com>
2023-05-23 11:47:10 -0500

    buqueda finished

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -18,11 +18,63 @@ const buscarUsuarios = async (termino = '', res = response) => {
         const usuario = await Usuarios.findById(termino);
         //res.json(usuario);
         res.json({
-            results: (usuario)?[usuario]:[]
+            results: (usuario) ? [usuario] : []
         });
     }
+
+    const regex = new RegExp(termino, 'i');
+
+    const usuarios = await Usuarios.find({
+        //$or: [{ nombre: regex, estado: true }, { correo: regex }]
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+    });
+
+    res.json({
+        results: usuarios
+    });
 }
 
+const buscarCategorias = async (termino = '', res = response) => {
+    const esMongoID = ObjectId.isValid(termino);//TRUE
+
+    if (esMongoID) {
+        const categoria = await Categoria.findById(termino);
+        res.json({
+            results: (categoria) ? [categoria] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const categorias = await Categoria.find({ nombre: regex, estado: true });
+
+    res.json({
+        results: categorias
+    });
+}
+
+const buscarProductos = async (termino = '', res = response) => {
+    const esMongoID = ObjectId.isValid(termino);//TRUE
+
+    if (esMongoID) {
+        const producto = await Producto.findById(termino)
+                            .populate('categoria', 'nombre');
+        //res.json(usuario);
+        res.json({
+            results: (producto) ? [producto] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const productos = await Producto.find({ nombre: regex, estado: true })
+                            .populate('categoria', 'nombre');
+
+    res.json({
+        results: productos
+    });
+}
 
 const buscar = (req, res = response) => {
 
@@ -37,16 +89,14 @@ const buscar = (req, res = response) => {
     switch (coleccion) {
         case 'usuarios':
             buscarUsuarios(termino, res);
-            /*res.json({
-                coleccion,
-                termino,
-            });*/
             break;
 
-        case 'categoria':
+        case 'categorias':
+            buscarCategorias(termino, res);
             break;
 
         case 'productos':
+            buscarProductos(termino, res);
             break;
 
         default:
